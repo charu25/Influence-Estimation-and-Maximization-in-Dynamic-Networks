3eabd003720c221f7adba0275255de630ce35ff1
SRIRAM <srirammanohar@gmail.com>
2017-05-06 18:15:32 -0700

    Done

---
+++ DynamicGraph.py
@@ -0,0 +1,45 @@
+class DynamicGraph(object):
+	Index = []
+	Vertices = {}
+	VtoIndices = { }
+	W = 0
+	IndegMap = {}
+	ps = {}
+	rs = {}
+	
+	def __init__(self):
+		self.name = ""
+
+	#u is the source ,v vertex and p is prob
+	def addEdge(u,v,p):
+		print "yafg"
+
+	def addVertex(u):
+		print "yafg"
+	
+
+	def delVertex(u):
+		print"ToDO"
+		
+	def delEdge(u,v):
+		print"ToDO"
+
+	def changeEdge(u,v,p):
+		print "To DO"
+
+	def _adjust():
+		print ""	
+
+	def _shrink(att):
+		print ""
+
+
+	def _expand(at, z):
+		print "To do"
+
+	def infEst():
+		print "to do"
+
+
+	def infMax():
+		print "To do"
